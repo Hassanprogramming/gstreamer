1c49e28889756eb5d53f4962b635381bbbccddbb
h.momeni <hassan.00.momeni@gmail.com>
2025-03-26 17:52:00 +0330

    add Extract the embedding from the frame

--- utils.py
+++ utils.py
@@ -2,8 +2,21 @@ import gi
 import numpy as np
 from gi.repository import Gst, GLib
 from PIL import Image as im
+# from tensorflow.keras.models import load_model
 
 gi.require_version("Gst", "1.0")
+
+# # Load the pre-trained FaceNet model
+# model = load_model("facenet_keras.h5")
+
+
+# # Function to extract embeddings using FaceNet
+# def get_embedding(model, face_pixels):
+#     face_pixels = face_pixels.astype('float32')
+#     mean, std = face_pixels.mean(), face_pixels.std()
+#     face_pixels = (face_pixels - mean) / std  # Normalize image
+#     face_pixels = np.expand_dims(face_pixels, axis=0)  # Add batch dimension
+#     return model.predict(face_pixels)[0]  # Return the embedding
 
 def initialize_gstreamer():
     """Initializes GStreamer."""
@@ -22,7 +35,7 @@ def create_pipeline():
     """Creates and returns a GStreamer pipeline with an autovideosink for debugging."""
     pipeline = Gst.parse_launch(
         "v4l2src ! decodebin ! videoconvert ! tee name=t "
-        "t. ! queue ! video/x-raw,format=BGR ! appsink name=sink emit-signals=true max-buffers=1 drop=true "
+        "t. ! queue ! video/x-raw,format=RGB ! appsink name=sink emit-signals=true max-buffers=1 drop=true "
         "t. ! queue ! videoconvert ! autovideosink"
     )
     return pipeline
@@ -46,6 +59,13 @@ def on_new_sample(sink):
     frame = np.frombuffer(map_info.data, dtype=np.uint8).reshape((height, width, 3))
     buffer.unmap(map_info)
     print("this is the frame shape == ", frame.shape)
+    
+    # # Preprocess the frame for FaceNet (resize to 160x160)
+    # # resized_frame = cv2.resize(frame, (160, 160))
+
+    # # Extract the embedding from the frame
+    # embedding = get_embedding(model, frame)
+    # print("Extracted Embedding:", embedding)
     
     # creating image object of above np array image
     data = im.fromarray(frame) 
